98f4c8577b8a945b59c5dacab8c72e6832924700
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2017-02-02 10:45:43 -0500

    Creando facories para categorias y productos, este factory es base solo para usar los test, mas adelante se modificaran

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,3 +17,15 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
     ];
 });
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name
+    ];
+});
